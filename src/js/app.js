66163b3097954a7d5b10c4ac439b648ef8f072c1
AlexAdutov <alexeyspark@yandex.ru>
2023-10-22 22:36:27 +0300

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,8 @@
-import Daemon from './daemon';
-import Magician from './magician';
-
-const magician = new Magician('Mag');
-
-const daemon = new Daemon('Devil');
-
-console.log(magician, daemon);
+// import Daemon from './daemon';
+// import Magician from './magician';
+//
+// const magician = new Magician('Mag');
+//
+// const daemon = new Daemon('Devil');
+//
+// console.log(magician, daemon);
